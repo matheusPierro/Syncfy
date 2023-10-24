a9b5d797ac2ad694969d9390927e8c86c5f93b44
matheusPierro <matpierro570@gmail.com>
2023-10-23 23:17:05 -0300

    Sprint 1 test

--- src/main/java/br/com/syncfy/domain/entity/CompanyUser.java
+++ src/main/java/br/com/syncfy/domain/entity/CompanyUser.java
@@ -7,9 +7,9 @@ import jakarta.persistence.*;
 public class CompanyUser {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_ID")
-    @SequenceGenerator(name = "SQ_ID", sequenceName = "SQ_ID")
-    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_COMPANY_USER")
+    @SequenceGenerator(name = "SQ_COMPANY_USER", sequenceName = "SQ_COMPANY_USER")
+    @Column(name = "ID_COMPANY_USER")
     private Long id;
 
     @Column(name = "CNPJ", nullable = false)
@@ -27,7 +27,81 @@ public class CompanyUser {
     @Column(name = "PASSWORD", nullable = false)
     private String password;
 
-    private Phone phone;
+    public CompanyUser() {
+    }
 
-    private Address address;
+    public CompanyUser(Long id, String cnpj, String name, boolean status, String email, String password) {
+        this.id = id;
+        this.cnpj = cnpj;
+        this.name = name;
+        this.status = status;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public CompanyUser setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public CompanyUser setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public CompanyUser setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public CompanyUser setStatus(boolean status) {
+        this.status = status;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public CompanyUser setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public CompanyUser setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "CompanyUser{" +
+                "id=" + id +
+                ", cnpj='" + cnpj + '\'' +
+                ", name='" + name + '\'' +
+                ", status=" + status +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
--- src/main/java/br/com/syncfy/domain/entity/Order.java
+++ src/main/java/br/com/syncfy/domain/entity/Order.java
@@ -2,16 +2,16 @@ package br.com.syncfy.domain.entity;
 
 import jakarta.persistence.*;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
-@Table(name = "ORDER")
+@Table(name = "TB_ORDER")
 public class Order {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_ID")
-    @SequenceGenerator(name = "SQ_ID", sequenceName = "SQ_ID")
-    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_ORDER")
+    @SequenceGenerator(name = "SQ_ORDER", sequenceName = "SQ_ORDER")
+    @Column(name = "ID_ORDER")
     private Long id;
 
     @Column(name = "CREATED_AT", nullable = false)
@@ -21,7 +21,7 @@ public class Order {
     private LocalDateTime updatedAt;
 
     @Column(name = "TOTAL_AMOUNT", nullable = false)
-    private double totalAmount;
+    private BigDecimal totalAmount;
 
     @Column(name = "DELIVERY_DATE")
     private LocalDateTime deliveryDate;
@@ -32,9 +32,172 @@ public class Order {
     @Column(name = "DESCRIPTION")
     private String description;
 
-    private List<Material> materials;
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(
+            name = "ID_VENDOR",
+            referencedColumnName = "ID_VENDOR",
+            foreignKey = @ForeignKey(name = "FK_VENDOR_ID_ORDER"),
+            nullable = false
+    )
+    private Vendor vendorId;
 
-    private ShippingCost shippingCost;
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(
+            name = "CNPJ_VENDOR",
+            referencedColumnName = "CNPJ",
+            foreignKey = @ForeignKey(name = "FK_VENDOR_CNPJ_ORDER"),
+            nullable = false
+    )
+    private Vendor vendorCnpj;
 
-    private Vendor vendor;
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(
+            name = "ID_COMPANY_USER",
+            referencedColumnName = "ID_COMPANY_USER",
+            foreignKey = @ForeignKey(name = "FK_COMPANY_USER_ID_ORDER"),
+            nullable = false
+    ) 
+    private CompanyUser companyUserId;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(
+            name = "CNPJ_COMPANY_USER",
+            referencedColumnName = "CNPJ",
+            foreignKey = @ForeignKey(name = "FK_COMPANY_USER_CNPJ_ORDER"),
+            nullable = false
+    )
+    private CompanyUser companyUserCnpj;
+
+    public Order() {
+    }
+
+    public Order(Long id, LocalDateTime createdAt, LocalDateTime updatedAt, BigDecimal totalAmount, LocalDateTime deliveryDate, String orderNumber, String description, Vendor vendorId, Vendor vendorCnpj, CompanyUser companyUserId, CompanyUser companyUserCnpj) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.totalAmount = totalAmount;
+        this.deliveryDate = deliveryDate;
+        this.orderNumber = orderNumber;
+        this.description = description;
+        this.vendorId = vendorId;
+        this.vendorCnpj = vendorCnpj;
+        this.companyUserId = companyUserId;
+        this.companyUserCnpj = companyUserCnpj;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Order setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public Order setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+        return this;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Order setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+        return this;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public Order setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+        return this;
+    }
+
+    public LocalDateTime getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public Order setDeliveryDate(LocalDateTime deliveryDate) {
+        this.deliveryDate = deliveryDate;
+        return this;
+    }
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public Order setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Order setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public Vendor getVendorId() {
+        return vendorId;
+    }
+
+    public Order setVendorId(Vendor vendorId) {
+        this.vendorId = vendorId;
+        return this;
+    }
+
+    public Vendor getVendorCnpj() {
+        return vendorCnpj;
+    }
+
+    public Order setVendorCnpj(Vendor vendorCnpj) {
+        this.vendorCnpj = vendorCnpj;
+        return this;
+    }
+
+    public CompanyUser getCompanyUserId() {
+        return companyUserId;
+    }
+
+    public Order setCompanyUserId(CompanyUser companyUserId) {
+        this.companyUserId = companyUserId;
+        return this;
+    }
+
+    public CompanyUser getcompanyUserCnpj() {
+        return companyUserCnpj;
+    }
+
+    public Order setCompanyUserC(CompanyUser companyUserCnpj) {
+        this.companyUserCnpj = companyUserCnpj;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                ", totalAmount=" + totalAmount +
+                ", deliveryDate=" + deliveryDate +
+                ", orderNumber='" + orderNumber + '\'' +
+                ", description='" + description + '\'' +
+                ", vendorId=" + vendorId +
+                ", vendorCnpj=" + vendorCnpj +
+                ", companyUserId=" + companyUserId +
+                ", companyUserCnpj=" + companyUserCnpj +
+                '}';
+    }
 }
